7ef2ae287ccfca0d29039fbf2503a419a1d4ad74
Seniru Abeywickrama <70276216+SeniruAbeywickrama@users.noreply.github.com>
2023-08-16 16:49:11 +0530

    response change

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const sequelize = require('./db');
 const User = require('./model/user');
 
 const {logEvent} = require('./logger');
+const {validationResult ,check} = require("express-validator");
 const env = require("dotenv").config();
 
 const app = express();
@@ -22,23 +23,6 @@ app.use(cors(
 app.use(express.json())
 app.use(cookieParser())
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: "",
-//     database: process.env.DB_NAME
-// })
-
-// db.connect((err) => {
-//     if(err){
-//         logEvent(err.message);
-//         return;
-//     }
-//     console.log("Database Connected to B2B.")
-//     createUserTable();
-//     createUserTable();
-// })
-/* Create User table */
 createUserTable();
 
 
@@ -81,30 +65,37 @@ app.post('/login', async (req, res) => {
     //         return  res.json("Fail")
     //     }
     // })
-    const options = {
-        where: {
-            email:  req.body.values.email[0],
-            password: req.body.values.password[0]
-        }
-    };
 
-    const user = await User.findOne(options);
-    try {
+    // const errors = validationResult(req);
+    //
+    // if(!errors.isEmpty()){
+    //     return res.status(400).json({errors: errors.array()});
+    // }
+    try{
+        const options = {
+            where: {
+                email:  req.body.values.email[0],
+                password: req.body.values.password[0]
+            }
+        };
+
+        const user = await User.findOne(options);
         if(!user) {
-            logEvent("Fail to LogIn");
-            return  res.json("Fail")
+            logEvent("No user exists");
+            return  res.status(500).json({message: 'No user exists',status: 500})
         }else {
             // const password_valid = await bcrypt.compare(req.body.values.password[0],user.password);
             const user_email = req.body.values.email[0];
+            const user_password = req.body.values.password[0];
             const company_token = jwt.sign({user_email},process.env.SECRET_KEY,{expiresIn: "1d"});
             res.cookie("company-token", company_token);
             logEvent("Successfully LogIn to the Company registration");
-            return  res.json(200)
+            return  res.status(200).json({message: 'Success', status: 200})
         }
     }catch (error) {
         console.error('Error creating table:', error);
+        return  res.status(404).json({message: 'NotFound', status: 404})
     }
-
 })
 
 
