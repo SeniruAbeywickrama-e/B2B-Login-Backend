3c1554c659eed6615fadedccb8f498d169a311b6
Seniru Abeywickrama <70276216+SeniruAbeywickrama@users.noreply.github.com>
2023-08-18 10:21:22 +0530

    make changes in server and userController

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,16 +1,13 @@
-const express = require('express');
 const bcrypt = require('bcryptjs');
-const config = require('config');
 const jwt = require('jsonwebtoken');
 const UserModel = require('../model/user');
 const {logEvent} = require('../logger');
 
-
 /* Function for login */
 const login = async (req, res) => {
     try {
-        const user_email = req.body.values.email[0];
-        const user_password = req.body.values.password[0];
+        const user_email = req.body.values.email;
+        const user_password = req.body.values.password;
 
         console.log(user_email)
 
@@ -26,13 +23,13 @@ const login = async (req, res) => {
 
             if (!user) {
                 logEvent("No user exists");
-                return res.status(500).json({message: 'No user exists', status: 500})
+                return res.status(200).json({message: 'No user exists', status: 404})
             } else {
                 // const password_valid = await bcrypt.compare(req.body.values.password[0],user.password);
-                const company_token = jwt.sign({user_email}, process.env.SECRET_KEY, {expiresIn: "1d"});
-                res.cookie("company-token", company_token);
+                const my_token = jwt.sign({user_email}, "our-secret-key-company",{expiresIn: "2h"});
+                res.cookie("tokenComp",my_token);
                 logEvent("Successfully LogIn to the Company registration");
-                return res.status(200).json({message: 'Success', status: 200})
+                return res.status(200).json({message: "Success", status: 200})
             }
         }else {
             return res.status(500).json({message: 'User Name or Password is Empty', status: 500})
@@ -40,7 +37,7 @@ const login = async (req, res) => {
 
     } catch (error) {
         console.error('Error creating table:', error);
-        return res.status(404).json({message: 'NotFound', status: 404})
+        return res.status(500).json({message: 'NotFound', status: 500})
     }
 }
 
--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const userRoute = require('./routes/auth');
 
 //
 // /* User Login */
-// app.post('/login', async (req, res) => {
+// app.post('/api/users/login', (req, res) => {
 //     // db.query("SELECT * FROM company WHERE `email` = ? AND `password`= ? " , [req.body.values.email[0],req.body.values.password[0]], (err, data) => {
 //     //     if(err){
 //     //         logEvent("Error on login");
@@ -78,21 +78,21 @@ const userRoute = require('./routes/auth');
 //     try{
 //         const options = {
 //             where: {
-//                 email:  req.body.values.email[0],
-//                 password: req.body.values.password[0]
+//                 email:  req.body.values.email,
+//                 password: req.body.values.password
 //             }
 //         };
 //
-//         const user = await User.findOne(options);
+//         const user = User.findOne(options);
 //         if(!user) {
 //             logEvent("No user exists");
 //             return  res.status(500).json({message: 'No user exists',status: 500})
 //         }else {
 //             // const password_valid = await bcrypt.compare(req.body.values.password[0],user.password);
-//             const user_email = req.body.values.email[0];
-//             const user_password = req.body.values.password[0];
-//             const company_token = jwt.sign({user_email},process.env.SECRET_KEY,{expiresIn: "1d"});
-//             res.cookie("company-token", company_token);
+//             const user_email = req.body.values.email;
+//             const user_password = req.body.values.password;
+//             const company_token = jwt.sign({user_email},"my-secret-key",{expiresIn: "1d"});
+//             res.cookie("company-token", company_token)
 //             logEvent("Successfully LogIn to the Company registration");
 //             return  res.status(200).json({message: 'Success', status: 200})
 //         }
