94ee8952e25c55b01606e9d810c3a0844c7f430f
Seniru Abeywickrama <70276216+SeniruAbeywickrama@users.noreply.github.com>
2023-09-07 09:05:43 +0530

    admin- search of investor tab and filter part done

--- controller/InvestorController.js
+++ controller/InvestorController.js
@@ -1,7 +1,7 @@
 const InvestorModel = require('../model/investor');
 const CompanyModel = require('../model/company');
 const {logEvent} = require('../logger');
-const {Sequelize, fn, col} = require("sequelize");
+const {Sequelize, fn, col , Op} = require("sequelize");
 
 
 const getAllInvestors = async (req, res) => {
@@ -58,19 +58,91 @@ const getInvestorById = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Internal Server Error', status: 500 });
     }
-
-
 }
 
 
+const searchInvestorByName = async (req, res) => {
+    const searchString = req.query.search;
+    console.log(searchString)
+    let params = {
+        attributes: [
+            [
+                Sequelize.fn('CONCAT', Sequelize.col('first_name'), ' ', Sequelize.col('last_name')),
+                'name'
+            ],
+            'investment_industry_preferences1', 'investor_type', 'investment_type', 'status', 'id'
+        ],
+        where: {
+            [Sequelize.Op.or]: {
+                'first_name': {
+                    [Sequelize.Op.like]: `%${searchString}%`
+                },
+                'last_name': {
+                    [Sequelize.Op.like]: `%${searchString}%`
+                }
+            },
+        },
+    }
 
+    try {
+        const data = await InvestorModel.findAll(params);
+        console.log(data)
+        return res.status(200).json(data);
 
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', status: 500 });
+    }
+}
 
+const investorFilter = async (req, res) => {
+    const filterIndustry = req.query.industry;
+    const filterInvestment = req.query.investment;
+    console.log(filterInvestment);
+    console.log(filterIndustry);
+    const params_f = {
+        attributes: [
+            [
+                Sequelize.fn('CONCAT', Sequelize.col('first_name'), ' ', Sequelize.col('last_name')),
+                'name'
+            ],
+            'investment_industry_preferences1', 'investor_type', 'investment_type', 'status', 'id'
+        ],
+        where: {
+            'investment_industry_preferences1': {
+                    [Op.like]: `${filterIndustry}%`
+            },
+            'investment_type': {
+                    [Op.like]: `${filterInvestment}%`
+            }
+        },
+    }
 
+    try {
+        const data = await InvestorModel.findAll(params_f);
+        console.log(data)
+        return res.status(200).json(data);
 
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', status: 500 });
+    }
+}
 
+const approveInvestor = async (req, res) => {
+    const filterIndustry = req.query.id;
 
+    try {
+        await InvestorModel.update({ status: 1 }, {
+            where: {
+                id: filterIndustry
+            }
+        });
+        return res.status(200).json({message: 'Successfully investor approved.',status: 200});
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', status: 500 });
+    }
+}
 
 module.exports = {
-    getAllInvestors,getInvestorById
+    getAllInvestors,getInvestorById,searchInvestorByName,investorFilter
 }
--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,8 @@ router.post('/register-company', RegisterController.registerCompany);
 
 router.get('/get-all-investors', InvestorController.getAllInvestors);
 router.get('/get-investors-by-id', InvestorController.getInvestorById);
+router.get('/investor/search', InvestorController.searchInvestorByName);
+router.get('/investor/filter', InvestorController.investorFilter);
 
 
 
